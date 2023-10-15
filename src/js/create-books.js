e55fd08a4b0713aea64282912769a4c4b64c11ff
Rizky Hamdani <rizkyhamdani67@gmail.com>
2023-10-16 05:02:36 +0800

    Parsing InputYear value type to number

--- src/js/create-books.js
+++ src/js/create-books.js
@@ -10,10 +10,15 @@ document.getElementById('form').addEventListener('submit', (event) => {
     // Get the Value from Client Side and store it in a Variable
     const title = document.getElementById('inputTitle').value;
     const author = document.getElementById('inputName').value;
-    const year = document.getElementById('inputYear').value;
+    const year = parseInt(document.getElementById('inputYear').value);
+    console.log(typeof(year));
     const id = helper.generateId();
     const newBooks = helper.generateBooksObject(id, title, author, year, false);
 
     // Creating new books and storing it to Local Storage
     handler.createNewBooks(newBooks, books);
+
+    document.getElementById('inputTitle').value = '';
+    document.getElementById('inputName').value = '';
+    document.getElementById('inputYear').value = '';
 })
